ee4794b485f26aac23d7a15dfcf0e019680e9c89
anishreddy1234 <smiles.anish@gmail.com>
2026-07-10 12:55:48 +0300

    Use Gmail SSL SMTP

--- backend/src/utils/otpService.js
+++ backend/src/utils/otpService.js
@@ -4,7 +4,7 @@ import { ApiError } from "./ApiError.js";
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 587,
+  port: 465,
   secure: false,
   auth: {
     user: process.env.EMAIL_USER,
